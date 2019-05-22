4d88181d6aaaa6fb79ce3b02e2b9150819ca7744
Hamza Serhane <hamza42700@gmail.com>
2019-05-22 15:56:01 +0200

    transfere du montage arduino sur nodemcu

--- memo_music/memo_music1.0.ino
+++ memo_music/memo_music1.0.ino
@@ -1,96 +1,4 @@
-/******MELODY-MARIO**********/
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
-
-#define melodyPin 3
-/******MELODY-MARIO**********/
+#include "pitches.h"
 
 int melody1[] = {
   NOTE_E7, NOTE_E7, 0, NOTE_E7,         //On définie première partie de la musique 
@@ -148,7 +56,7 @@ int noteDurations[]= {
   12, 12, 12, 12,
 };
 
-int sequence[5]; //on ne dépasse pas la partie 5
+//int sequence[5]; //on ne dépasse pas la partie 5 : quelle utilité ?
 
 //On définie les boutons
 int button1 = 16;
@@ -165,8 +73,8 @@ void setup() {
   pinMode(button3, INPUT);
   pinMode(button4, INPUT);
   pinMode(button5, INPUT);
-//  pinMode(button6, INPUT);
-//  pinMode(buzz, OUTPUT);
+  //pinMode(button6, INPUT);
+  //pinMode(buzz, OUTPUT);
 }
 
 void loop(){
@@ -174,18 +82,18 @@ void loop(){
   //On lit la broche d'entrée
   int buttonEtat = digitalRead(button1);
  
-  //Si le bouton et pressée
-  if (buttonEtat == 1){
+  //Si le bouton est pressé
+  if (buttonEtat == 0){
  
-    //On parcour les notes de la mélodie
+    //On parcourt les notes de la mélodie
     for (int thisNote=0; thisNote <12; thisNote++){
  
       int noteDuration = 1500 / noteDurations [thisNote];       //On prend une seconde pour calculer la durée de la note
       tone(12, melody1 [thisNote], noteDuration);
  
-       //On défini un délai minimal entre elles
-      int pauseBetweenNotes = noteDuration * 1.30;   //la durée de la note + 30% semble bien fonctionner
-      delay(pauseBetweenNotes);
+       //On définit un délais minimum entre les notes
+      int pauseEntreNotes = noteDuration * 1.30;   //la durée de la note + 30% semble bien fonctionner
+      delay(pauseEntreNotes);
  
       //stop music
       noTone(12);
@@ -194,7 +102,7 @@ void loop(){
   /***************************************************************/
  /*ON DONNE LES MEMES CONDITIONS AU BOUTON 2 COMME POUR LE BOUTON 1*/
   /**************************************************************/
-   buttonEtat = digitalRead(button2);
+  // buttonEtat = digitalRead(button2);
  
   if (buttonEtat == 1){
  
@@ -203,8 +111,8 @@ void loop(){
       int noteDuration = 1500 / noteDurations [thisNote];      
       tone(12, melody2 [thisNote], noteDuration);
  
-      int pauseBetweenNotes = noteDuration * 1.30;  
-      delay(pauseBetweenNotes);
+      int pauseEntreNotes = noteDuration * 1.30;  
+      delay(pauseEntreNotes);
  
       noTone(12);
     }
@@ -212,7 +120,7 @@ void loop(){
   /***************************************************************/
  /*ON DONNE LES MEMES CONDITIONS AU BOUTON 3 COMME POUR LE BOUTON 1*/
   /**************************************************************/
-   buttonEtat = digitalRead(button3);
+   //buttonEtat = digitalRead(button3);
  
   if (buttonEtat == 1){
  
@@ -221,8 +129,8 @@ void loop(){
       int noteDuration = 1500 / noteDurations [thisNote];      
       tone(12, melody3 [thisNote], noteDuration);
   
-      int pauseBetweenNotes = noteDuration * 1.30; 
-      delay(pauseBetweenNotes);
+      int pauseEntreNotes = noteDuration * 1.30; 
+      delay(pauseEntreNotes);
  
       //stop music
       noTone(12);
@@ -231,7 +139,7 @@ void loop(){
   /***************************************************************/
  /*ON DONNE LES MEMES CONDITIONS AU BOUTON 4 COMME POUR LE BOUTON 1*/
   /**************************************************************/
-   buttonEtat = digitalRead(button4);
+  // buttonEtat = digitalRead(button4);
  
   
   if (buttonEtat == 1){
@@ -243,8 +151,8 @@ void loop(){
       tone(12, melody4 [thisNote], noteDuration);
  
        
-      int pauseBetweenNotes = noteDuration * 1.30;   
-      delay(pauseBetweenNotes);
+      int pauseEntreNotes = noteDuration * 1.30;   
+      delay(pauseEntreNotes);
  
       //stop music
       noTone(12);
@@ -253,7 +161,7 @@ void loop(){
   /***************************************************************/
  /*ON DONNE LES MEMES CONDITIONS AU BOUTON 5 COMME POUR LE BOUTON 1*/
   /**************************************************************/
-  buttonEtat = digitalRead(button5);
+// buttonEtat = digitalRead(button5);
  
 
   if (buttonEtat == 1){
@@ -265,10 +173,11 @@ void loop(){
       tone(12, melody5 [thisNote], noteDuration);
  
        //On défini un délai minimal entre elles
-      int pauseBetweenNotes = noteDuration * 1.30;   
-      delay(pauseBetweenNotes);
+      int pauseEntreNotes = noteDuration * 1.30;   
+      delay(pauseEntreNotes);
  
       noTone(12);
     }
   }
+  
 }
